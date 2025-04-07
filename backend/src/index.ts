8b5accfe24a9b39613fe5dc0fd7135431a966d2f
IshanHunt77 <ishanraghuvanshi046@gmail.com>
2025-04-07 14:04:48 +0530

    beta2

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -21,7 +21,7 @@ app.use('/uploads', express.static('uploads'));
 app.use('/uploadProfile', express.static('uploadProfile'));
 
 app.use(cors({
-    origin: "http://localhost:5173", 
+    origin: ["http://localhost:5173", "https://writely3-0-1-web.onrender.com"], 
     credentials: true
 }));
 app.use("/signup", signupRoute);
